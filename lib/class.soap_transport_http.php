345c8ae1fe0dc91c9fbfc35e3064b42a9d3c1b80
Romain HONEL <romain.honel@gmail.com>
2016-03-07 09:44:58 +0100

    Fix isSkippableCurlHeader()

--- lib/class.soap_transport_http.php
+++ lib/class.soap_transport_http.php
@@ -610,7 +610,7 @@ class soap_transport_http extends nusoap_base {
                                         'HTTP/1.0 401',
                                         'HTTP/1.1 401',
                                         'HTTP/1.0 200 Connection established',
-                                        'HTTP/1.1 200 Connection Established');
+                                        'HTTP/1.1 200 Connection established');
 		foreach ($skipHeaders as $hd) {
 			$prefix = substr($data, 0, strlen($hd));
 			if ($prefix == $hd) return true;
